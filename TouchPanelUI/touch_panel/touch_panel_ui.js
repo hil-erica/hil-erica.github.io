d05de51987cc37b65d81b0b011258fb0c97ef77f
tomo-ff <funayama.tomo@gmail.com>
2024-02-01 18:55:49 +0900

    選択肢ボタンの表示を調整。

--- TouchPanelUI/touch_panel/touch_panel_ui.js
+++ TouchPanelUI/touch_panel/touch_panel_ui.js
@@ -13,24 +13,25 @@ function connectToIshiki(){
 
 	//接続通知
 	connection.onopen = function(event) {
-		document.getElementById( "eventType" ).value = "通信接続イベント受信";
-		document.getElementById( "dispMsg" ).value = event.data;
+		// document.getElementById( "eventType" ).value = "通信接続イベント受信";
+		// document.getElementById( "dispMsg" ).value = event.data;
 	};
 
 	//エラー発生
 	connection.onerror = function(error) {
-		document.getElementById( "eventType" ).value = "エラー発生イベント受信";
-		document.getElementById( "dispMsg" ).value = error.data;
+		// document.getElementById( "eventType" ).value = "エラー発生イベント受信";
+		// document.getElementById( "dispMsg" ).value = error.data;
 	};
 
 	//メッセージ受信
 	connection.onmessage = function(event) {
-		document.getElementById( "eventType" ).value = "メッセージ受信";
-		document.getElementById( "received" ).value = event.data;
+		// document.getElementById( "eventType" ).value = "メッセージ受信";
+		// document.getElementById( "received" ).value = event.data;
 		const receivedCommand = JSON.parse(event.data);
 		console.log(receivedCommand.op);
 		console.log(receivedCommand.choices);
 
+		clearChoiceButtons();
 		for (var i = 0; i < receivedCommand.choices.length; i++) {
 			let element = receivedCommand.choices[i];
 			console.log(element);
@@ -40,25 +41,54 @@ function connectToIshiki(){
 
 	//切断
 	connection.onclose = function() {
-		document.getElementById( "eventType" ).value = "通信切断イベント受信";
-		document.getElementById( "dispMsg" ).value = "";
+		// document.getElementById( "eventType" ).value = "通信切断イベント受信";
+		// document.getElementById( "dispMsg" ).value = "";
 	};
 }
 
-function createChoiceButton(choice_text, choice_index) {
-	let button = document.createElement("button");
-
-	button.innerHTML = choice_text;
-
-	button.id = "choice_button_" + choice_index;
-
-	let target = document.getElementById("choice_buttons");
-	button.addEventListener("click", callback);
-
-	target.appendChild(button);
+function clearChoiceButtons() {
+	let target = document.getElementById("choice_buttons_panel");
+	while(target.firstChild){
+		target.removeChild(target.firstChild);
+	}
+}
+function fadeOutChoiceButtons() {
+	let target = document.getElementById("choice_buttons_panel");
+	for (var i = 0; i < target.children.length; i++) {
+		console.log(target.children[i]);
+		target.children[i].classList.add('is-hide');
+		// target.children[i].firstChild.disabled = true;
+	}
 }
 
-function callback(e) {
+function createChoiceButton(choice_text, choice_index) {
+	var div = document.createElement('div');
+	div.className = 'choice_button';
+	var div_text = document.createElement('div');
+	div_text.className = 'choice_button_text';
+	// div_text.innerHTML = "hjge";
+
+	// let button = document.createElement("button");
+	// button.innerHTML = choice_text;
+	// button.id = "choice_button_" + choice_index;
+
+	let button = document.createElement("a");
+	// button.className = 'btn btn--orange';
+	button.className = 'choice_button_text';
+	button.innerHTML = choice_text;
+	button.id = "choice_button_" + choice_index;
+
+	let target = document.getElementById("choice_buttons_panel");
+	div.addEventListener("click", onButtonClicked);
+	// button.classList.toggle('is-show');
+
+	div.appendChild(button);
+	// div_text.appendChild(button);
+	target.appendChild(div);
+	// button.classList.add('is-show');
+}
+
+function onButtonClicked(e) {
 	console.log(e.target.id);
 
 	let choice_index = parseInt(e.target.id.replace("choice_button_", ""), 10);
@@ -66,6 +96,9 @@ function callback(e) {
 	let returnCommand = JSON.stringify(returnCommandData);
 	console.log(returnCommand);
 	connection.send(returnCommand);
+	// clearChoiceButtons();
+	fadeOutChoiceButtons();
+	// e.target.classList.add('is-hide');
 }
 
 
@@ -76,5 +109,7 @@ function onClicked(){
 window.addEventListener('load', function () {
 	connectToIshiki();
 
-	// createChoiceButton("hoge", 2);
+	createChoiceButton("hoge0", 0);
+	createChoiceButton("hoge1", 1);
+	createChoiceButton("hoge2", 2);
 });
