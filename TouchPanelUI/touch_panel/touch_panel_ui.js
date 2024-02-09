1bec710fe1de9ca71af6be34a17b3edc9f67f174
tomo-ff <funayama.tomo@gmail.com>
2024-02-09 20:07:11 +0900

    説明文画面分割と言語選択ボタンの追加

--- TouchPanelUI/touch_panel/touch_panel_ui.js
+++ TouchPanelUI/touch_panel/touch_panel_ui.js
@@ -5,11 +5,14 @@ var doUpdate = false;
 
 var connection;
 
+/*
+	ISHIKIのWebSocketサーバに接続
+*/
 function connectToIshiki(){
 	// console.log("hogehoghoe");
 
 	//WebSocket接続
-	connection = new WebSocket("ws://localhost:8000/");
+	connection = new WebSocket("ws://localhost:8800/");
 
 	//接続通知
 	connection.onopen = function(event) {
@@ -24,20 +27,11 @@ function connectToIshiki(){
 	};
 
 	//メッセージ受信
-	connection.onmessage = function(event) {
-		// document.getElementById( "eventType" ).value = "メッセージ受信";
-		// document.getElementById( "received" ).value = event.data;
-		const receivedCommand = JSON.parse(event.data);
-		console.log(receivedCommand.op);
-		console.log(receivedCommand.choices);
-
-		clearChoiceButtons();
-		for (var i = 0; i < receivedCommand.choices.length; i++) {
-			let element = receivedCommand.choices[i];
-			console.log(element);
-			createChoiceButton(element, i);
-		}
-	};
+	// connection.onmessage = function(event) {
+	// 	// document.getElementById( "eventType" ).value = "メッセージ受信";
+	// 	// document.getElementById( "received" ).value = event.data;
+	// };
+	connection.onmessage = processReceivedMessage;
 
 	//切断
 	connection.onclose = function() {
@@ -45,6 +39,33 @@ function connectToIshiki(){
 		// document.getElementById( "dispMsg" ).value = "";
 	};
 }
+
+
+function processReceivedMessage(event) {
+	console.log("processReceivedMessage");
+	const receivedCommand = JSON.parse(event.data);
+	console.log(receivedCommand.op);
+	console.log(receivedCommand.choices);
+	console.log(receivedCommand.description);
+
+	if (receivedCommand.op == 'set_choices') {
+		if (receivedCommand.description_en === undefined) {
+			console.log("description undefined");
+		}
+		else {
+			console.log("description not undefined");
+		}
+		clearChoiceButtons();
+		for (var i = 0; i < receivedCommand.choices.length; i++) {
+			let element = receivedCommand.choices[i];
+			console.log(element);
+			createChoiceButton(element, i);
+		}
+	}
+
+}
+
+
 
 function clearChoiceButtons() {
 	let target = document.getElementById("choice_buttons_panel");
@@ -61,55 +82,89 @@ function fadeOutChoiceButtons() {
 	}
 }
 
-function createChoiceButton(choice_text, choice_index) {
+function createDescriptionContent(description) {
 	var div = document.createElement('div');
-	div.className = 'choice_button';
-	var div_text = document.createElement('div');
-	div_text.className = 'choice_button_text';
+	div.className = 'description_content';
+	div.id = "description_content";
+	div.innerHTML = description;
+
+	// let text = document.createElement('div');
+	// text.className = 'description_text';
+	// text.innerHTML = description;
+
+	// div.appendChild(text);
+	let target = document.getElementById("description_panel");
+	target.appendChild(div);
+}
+function createChoiceButton(choice_text, choice_index) {
+	// var div = document.createElement('div');
+	// div.className = 'choice_button';
+	// div.id = "choice_button_" + choice_index;
+	// var div_text = document.createElement('div');
+	// div_text.className = 'choice_button_text';
 	// div_text.innerHTML = "hjge";
 
-	// let button = document.createElement("button");
-	// button.innerHTML = choice_text;
-	// button.id = "choice_button_" + choice_index;
-
 	let button = document.createElement("a");
-	// button.className = 'btn btn--orange';
-	button.className = 'choice_button_text';
+	// button.className = 'choice_button_text';
+	button.className = 'choice_button';
 	button.innerHTML = choice_text;
 	button.id = "choice_button_" + choice_index;
 
-	let target = document.getElementById("choice_buttons_panel");
-	div.addEventListener("click", onButtonClicked);
+	button.addEventListener("click", onButtonClicked);
 	// button.classList.toggle('is-show');
 
-	div.appendChild(button);
+	// div.appendChild(button);
 	// div_text.appendChild(button);
-	target.appendChild(div);
+
+	let target = document.getElementById("choice_buttons_panel");
+	target.appendChild(button);
 	// button.classList.add('is-show');
 }
 
 function onButtonClicked(e) {
-	console.log(e.target.id);
-
+	// console.log("e is : " + e.target.id);
 	let choice_index = parseInt(e.target.id.replace("choice_button_", ""), 10);
 	let returnCommandData = {op: 'selected', selected: choice_index};
 	let returnCommand = JSON.stringify(returnCommandData);
 	console.log(returnCommand);
 	connection.send(returnCommand);
 	// clearChoiceButtons();
-	fadeOutChoiceButtons();
+	// fadeOutChoiceButtons();
 	// e.target.classList.add('is-hide');
+
+	createChoiceButtonsOnlyScreen();
 }
 
 
-function onClicked(){
-	connection.send('サンプルデータ');
+/*
+	画面を説明と選択肢に分割
+*/
+function createDescriptionAndChoiceButtonsScreen() {
+	let description_panel = document.getElementById("description_panel");
+	description_panel.style.height = 'calc(100% / 3 - 0px)';
+	let choice_buttons_panel = document.getElementById("choice_buttons_panel");
+	choice_buttons_panel.style.height = 'calc(100% * 2 / 3 - 0px)';
 }
+
+/*
+	画面を選択肢表示のみに
+*/
+function createChoiceButtonsOnlyScreen() {
+	let description_panel = document.getElementById("description_panel");
+	description_panel.style.height = '0%';
+	let choice_buttons_panel = document.getElementById("choice_buttons_panel");
+	choice_buttons_panel.style.height = '100%';
+}
+
 
 window.addEventListener('load', function () {
 	connectToIshiki();
 
+	createDescriptionContent("これはテスト説明です。");
 	createChoiceButton("hoge0", 0);
 	createChoiceButton("hoge1", 1);
 	createChoiceButton("hoge2", 2);
+	createChoiceButton("hoge3", 3);
+	createDescriptionAndChoiceButtonsScreen();
+	// createChoiceButtonsOnlyScreen();
 });
