b471ffcad6b38b223d8935a4430c90859d4a2d9f
tomo-ff <funayama.tomo@gmail.com>
2024-02-20 18:04:19 +0900

    サーバURLのユーザ入力を追加、説明だけ表示の処理を追加、選択肢ボタンの数に応じてサイズ変更を追加、ボタンフェードアウト後にオブジェクト削除を追加

--- TouchPanelUI/touch_panel/touch_panel_ui.js
+++ TouchPanelUI/touch_panel/touch_panel_ui.js
@@ -6,14 +6,32 @@ var choices_jp;
 var description_en;
 var choices_en;
 
+
+/*
+	入力ダイアログを表示して接続先urlを取得
+*/
+function getIshikiUrlFromUser(){
+	// 入力ダイアログを表示
+	url = window.prompt("ISHIKIサーバurlを指定してください", "ws://localhost:8800/");
+	return url;
+}
+
+
 /*
 	ISHIKIのWebSocketサーバに接続
 */
 function connectToIshiki(){
-	// console.log("hogehoghoe");
+	// urlをユーザから取得
+	url = getIshikiUrlFromUser();
+
+	// 空の場合やキャンセルした場合
+	if(url == null) {
+		// window.alert('キャンセルされました');
+		return;
+	}
 
 	//WebSocket接続
-	connection = new WebSocket("ws://localhost:8800/");
+	connection = new WebSocket(url);
 
 	//接続通知
 	connection.onopen = function(event) {
@@ -43,6 +61,9 @@ function connectToIshiki(){
 }
 
 
+/*
+	ISHIKIからのメッセージパース
+*/
 function processReceivedMessage(event) {
 	console.log("processReceivedMessage");
 	const receivedCommand = JSON.parse(event.data);
@@ -52,34 +73,55 @@ function processReceivedMessage(event) {
 	console.log(receivedCommand.choices_en);
 	console.log(receivedCommand.description_en);
 
+	// 全データをグローバルに入れておく
+	description_jp = receivedCommand.description;
+	description_en = receivedCommand.description_en;
+	choices_jp = receivedCommand.choices;
+	choices_en = receivedCommand.choices_en;
+
+	// 現在の言語設定で使用するデータを選択
+	if (current_language === 'jp') {
+		target_description = description_jp;
+		target_choices = choices_jp;
+	}
+	else if (current_language === 'en') {
+		target_description = description_en;
+		target_choices = choices_en;
+	}
+
+	// コマンドごとに処理
 	if (receivedCommand.op == 'set_choices') {
 		// description
-		description_jp = receivedCommand.description;
-		description_en = receivedCommand.description_en;
-
 		clearDescriptionContent();
+		// そもそもdescriptionがなければ、選択肢だけ表示する
 		if (receivedCommand.description !== undefined) {
 			console.log("description not undefined");
 			createDescriptionAndChoiceButtonsScreen();
-			createDescriptionContent(description_jp);
+			createDescriptionContent(target_description);
 		}
 		else{
 			createChoiceButtonsOnlyScreen();
 		}
 
 		// choices
-		choices_jp = receivedCommand.choices;
-		choices_en = receivedCommand.choices_en;
 		clearChoiceButtons();
-		for (var i = 0; i < receivedCommand.choices.length; i++) {
-			let choice = receivedCommand.choices[i];
+		for (var i = 0; i < target_choices.length; i++) {
+			let choice = target_choices[i];
 			console.log(choice);
 			createChoiceButton(choice, i);
 		}
+		adjustChoiceButtonsSize();
 	}
-
+	else if (receivedCommand.op == 'set_description') {
+		clearDescriptionContent();
+		createDescriptionOnlyScreen();
+		createDescriptionContent(target_description);
+	}
 }
 
+/*
+	言語切替
+*/
 function toggleLanguage() {
 	let target_description;
 	let target_choices;
@@ -114,27 +156,57 @@ function toggleLanguage() {
 }
 
 
+/*
+	説明文オブジェクトを削除
+*/
 function clearDescriptionContent() {
 	let target = document.getElementById("description_panel");
 	while(target.firstChild){
 		target.removeChild(target.firstChild);
 	}
 }
+/*
+	選択肢ボタンオブジェクトを削除
+*/
 function clearChoiceButtons() {
 	let target = document.getElementById("choice_buttons_panel");
 	while(target.firstChild){
 		target.removeChild(target.firstChild);
 	}
 }
+
+/*
+	説明文オブジェクトをフェードアウト
+*/
+function fadeOutDescriptionContent() {
+	let target = document.getElementById("description_panel");
+	for (var i = 0; i < target.children.length; i++) {
+		// console.log(target.children[i]);
+		target.children[i].classList.add('is-hide');
+		// target.children[i].firstChild.disabled = true;
+	}
+}
+/*
+	選択肢ボタンオブジェクトをフェードアウト
+*/
 function fadeOutChoiceButtons() {
 	let target = document.getElementById("choice_buttons_panel");
 	for (var i = 0; i < target.children.length; i++) {
-		console.log(target.children[i]);
+		if (i == 0) {
+			target.children[i].addEventListener('transitionend', () => {
+				// アニメーション終了後にボタンを消す
+				clearChoiceButtons();
+			})
+		}
+		// console.log(target.children[i]);
 		target.children[i].classList.add('is-hide');
 		// target.children[i].firstChild.disabled = true;
 	}
 }
 
+/*
+	説明文オブジェクトを生成
+*/
 function createDescriptionContent(description) {
 	var div = document.createElement('div');
 	div.className = 'description_content';
@@ -149,6 +221,9 @@ function createDescriptionContent(description) {
 	let target = document.getElementById("description_panel");
 	target.appendChild(div);
 }
+/*
+	選択肢ボタンオブジェクトを生成
+*/
 function createChoiceButton(choice_text, choice_index) {
 	// var div = document.createElement('div');
 	// div.className = 'choice_button';
@@ -174,6 +249,10 @@ function createChoiceButton(choice_text, choice_index) {
 	// button.classList.add('is-show');
 }
 
+
+/*
+	選択肢ボタンクリックイベント
+*/
 function onButtonClicked(e) {
 	// console.log("e is : " + e.target.id);
 	let choice_index = parseInt(e.target.id.replace("choice_button_", ""), 10);
@@ -183,12 +262,58 @@ function onButtonClicked(e) {
 	connection.send(returnCommand);
 	// clearChoiceButtons();
 	fadeOutChoiceButtons();
+	fadeOutDescriptionContent();
 	// e.target.classList.add('is-hide');
 	// createChoiceButtonsOnlyScreen();
 }
 
+/*
+	言語ボタンクリックイベント
+*/
 function onLanguageButtonClicked(e) {
 	toggleLanguage();
+}
+
+
+/*
+	選択肢ボタンのサイズをボタンの数に応じて調整
+*/
+function adjustChoiceButtonsSize() {
+	const target = document.getElementById("choice_buttons_panel");
+	let row_num = getChoiceButtonsRawNum(target.children.length);
+	let column_num = getChoiceButtonsColumnNum(target.children.length, row_num);
+	console.log("row_num : " + row_num);
+	console.log("column_num : " + column_num);
+	for (let button of target.children) {
+		console.log("button");
+		button.style.width = `calc(100% / ${column_num} - 20px)`;
+		button.style.height = `calc(100% / ${row_num} - 20px)`;
+	}
+}
+/*
+	選択肢ボタン配置の行の数を取得
+*/
+function getChoiceButtonsRawNum(choice_num) {
+	if (choice_num <= 2) {
+		return 1;
+	}
+	if (choice_num <= 8) {
+		return 2;
+	}
+	return 3;
+}
+/*
+	選択肢ボタン配置の列の数を取得
+*/
+function getChoiceButtonsColumnNum(choice_num, row_num) {
+	if (choice_num == 1) {
+		return 1;
+	}
+	let column_num = parseInt(choice_num / row_num);
+	if (choice_num % 2 != 0) {
+		column_num++;
+	}
+	return column_num;
 }
 
 /*
@@ -200,11 +325,21 @@ function createDescriptionAndChoiceButtonsScreen() {
 	let choice_buttons_panel = document.getElementById("choice_buttons_panel");
 	choice_buttons_panel.style.height = 'calc(100% * 2 / 3 - 0px)';
 }
-
+/*
+	画面を説明表示のみに
+*/
+function createDescriptionOnlyScreen() {
+	let description_panel = document.getElementById("description_panel");
+	description_panel.style.height = '100%';
+	clearChoiceButtons();
+	let choice_buttons_panel = document.getElementById("choice_buttons_panel");
+	choice_buttons_panel.style.height = '0%';
+}
 /*
 	画面を選択肢表示のみに
 */
 function createChoiceButtonsOnlyScreen() {
+	clearDescriptionContent();
 	let description_panel = document.getElementById("description_panel");
 	description_panel.style.height = '0%';
 	let choice_buttons_panel = document.getElementById("choice_buttons_panel");
@@ -221,8 +356,14 @@ window.addEventListener('load', function () {
 	createDescriptionContent("これはテスト説明です。");
 	createChoiceButton("hoge0", 0);
 	createChoiceButton("hoge1", 1);
-	createChoiceButton("hoge2", 2);
-	createChoiceButton("hoge3", 3);
+	// createChoiceButton("hoge2", 2);
+	// createChoiceButton("hoge3", 3);
+	// createChoiceButton("hoge4", 4);
+	// createChoiceButton("hoge4", 4);
+
+	adjustChoiceButtonsSize();
+	
+	// createDescriptionOnlyScreen();
 	createDescriptionAndChoiceButtonsScreen();
 	// createChoiceButtonsOnlyScreen();
 });
